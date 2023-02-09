cd52bd15ee90aa4a72a01d787849a1c03dbf30b8
Rakibsayyed <124577585+Rakibsayyed@users.noreply.github.com>
2023-02-09 17:56:30 +0530

    Add files via upload

---
+++ Language basics/CONTINUE.php
@@ -0,0 +1,8 @@
+<?php
+for($i=12;$i<=190;$i++){
+if($i == 90){
+    continue; //continue statement to break one iteration.in this code 90 is skiped
+}
+
+    echo"$i".PHP_EOL;
+}
---
+++ Language basics/days_switch.php
@@ -0,0 +1,30 @@
+<?php
+$day="mon";
+switch($day){
+case "mon";
+echo "monday";
+break;
+case "tue";
+echo "tuesday";
+break;
+case "wed";
+echo "wednesday";
+break;
+case "thur";
+echo "thursday";
+break;
+case "fri";
+echo "friday";
+break;
+case "sat";
+echo "saturday";
+break;
+case "sun";
+echo "sunday";
+break;
+default:
+    echo ("non of the days above");
+
+
+
+}
