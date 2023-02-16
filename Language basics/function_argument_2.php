c2e3e5d1ae06b8198d56b125a9fe28780420ed7b
Rakibsayyed <124577585+Rakibsayyed@users.noreply.github.com>
2023-02-16 18:11:00 +0530

    Add files via upload

---
+++ Language basics/function_argument_2.php
@@ -0,0 +1,10 @@
+<?php
+function person($name,$age,$address)
+{
+echo"hi may name is $name ,i am $age  years old & i am from $address".PHP_EOL;
+
+}
+person("karan",19,"jharkhand");
+person("kunal",33,"dehli");
+person("ravi",24,"Pune");
+?>
