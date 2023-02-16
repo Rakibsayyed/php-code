4a83e7d93914d88eb6c56a03f60a71a18bd1960f
Rakibsayyed <124577585+Rakibsayyed@users.noreply.github.com>
2023-02-16 18:03:54 +0530

    Add files via upload

---
+++ Language basics/User_defined_function.php
@@ -0,0 +1,10 @@
+<?php
+function definition()
+{
+    echo"PHP function is a piece of code that can be reused many times. 
+    It can take input as argument list and return value. 
+    There are thousands of built-in functions in PHP.";
+}
+definition();
+
+?>
---
+++ Language basics/function_argument.php
@@ -0,0 +1,8 @@
+<?php
+function hi($name)
+{
+    echo "hi $name".PHP_EOL;
+    }
+    hi("razz");
+    hi("aman");
+    ?>
